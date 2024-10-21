d56d15d01057ad3f0c7079f344a32392b4323f2a
David Maraver <dmaraver89@gmail.com>
2024-10-21 17:02:48 +0200

    Esto es un comentario para añadir

--- Practica01.py
+++ Practica01.py
@@ -3,12 +3,40 @@ esto es para
 usarlo para
 varias lineas
 """
+#REALIZADO POR DAVID MARAVER CEBALLOS
 
 print(texto)
 
-s=input("Dime tu nombre")
+
+#PARTE 2
+s=input("Dime tu nombre: ")
 print(f"Hola  {s}")
+
+
+#PARTE 3
+print(f"Hola {s.upper()}")
+
+print(f"{len(s)}")
+
+
+#PARTE 4
+numeropar=input("Dime un numero: ")
+for i in range (0,numeropar):
+    print(i+2)
+
+
+print(f"")
+
+
+
+
+
+
 try:
-    num=int(input("Dime un numero"))
+    num=int(input("Dime un numero: "))
 except:
     print ("fallo")
+
+print(f"el numero puesto es {num}")
+
+
